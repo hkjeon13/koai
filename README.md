6a4ba5a22533aa2868fb0f338293c470a9091206
eddiejeon <eddie14@wisenut.co.kr>
2022-10-07 22:07:30 +0900

    init

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-## KorAI; Korean AI. 한국어를 위한 인공지능 프로젝트
+## KorAI; Korean AI Project. 한국어를 위한 인공지능 프로젝트
 
 ---
+
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", mode="r", encoding="utf-8") as readme:
+    long_description = readme.read()
+
+
+setup(
+    name='korai',
+    version='0.0.0.1',
+    description='Korean AI Project',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hkjeon13/KorAI",
+    author="Eddie",
+    author_email="hkjeo13@gmail.com",
+    zip_safe=False,
+    license="MIT",
+
+    py_modules=["korai"],
+
+    python_requires=">=3",
+
+    packages=["korai"],
+    include_package_data=True,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+)
