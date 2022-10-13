14b0d412154a8a17c5f738fad86ec3e4c5f355df
eddiejeon <eddie14@wisenut.co.kr>
2022-10-13 09:57:41 +0900

    finetune

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
     python_requires=">=3",
 
     packages=find_packages("./"),
+    package_data= {"koai": ["*.json"]},
     include_package_data=True,
     install_requires=[
             "transformers",
