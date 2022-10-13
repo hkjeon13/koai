33c2cad24dc9865c107e1ebe2bef61d6133e107c
eddiejeon <eddie14@wisenut.co.kr>
2022-10-13 09:29:27 +0900

    finetune

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
 
     python_requires=">=3",
 
-    packages=["koai"],
+    packages=find_packages("koai"),
     include_package_data=True,
     install_requires=[
             "transformers",
