89b535b0eb9fc9884cfbae6c84e546fe30420c70
psyche <hkjeon13@gmail.com>
2022-11-25 12:51:29 +0900

    fix example function

--- koai/__init__.py
+++ koai/__init__.py
@@ -1,6 +1,6 @@
 from . import benchmarks
 from .benchmarks import finetune
 
-__version__ = "0.0.0.7"
+__version__ = "0.0.0.8"
 
 __all__ = ["benchmarks", "finetune"] # more functions will be added soon...
