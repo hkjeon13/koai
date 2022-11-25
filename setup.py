51cbeede2794a1c280e20a470be81b7c58610239
psyche <hkjeon13@gmail.com>
2022-11-25 12:57:12 +0900

    fix example function

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", mode="r", encoding="utf-8") as readme:
 
 setup(
     name='koai',
-    version='0.0.0.8',
+    version='0.0.0.9',
     description='Korean AI Project',
     long_description=long_description,
     long_description_content_type="text/markdown",
