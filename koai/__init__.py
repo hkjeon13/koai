513c5205a38f73d8e387976ed4ad0997d0951707
eddiejeon <eddie14@wisenut.co.kr>
2022-11-27 23:03:11 +0900

    fix-error

--- koai/__init__.py
+++ koai/__init__.py
@@ -1,6 +1,6 @@
 from . import benchmarks
 from .benchmarks import finetune
 
-__version__ = "0.0.1.1"
+__version__ = "0.0.1.2"
 
 __all__ = ["benchmarks", "finetune"] # more functions will be added soon...
