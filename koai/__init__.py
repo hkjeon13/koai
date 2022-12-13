1e7004f1f9e9594218f313a8cd2e888c3959f0ae
psyche <hkjeon13@gmail.com>
2022-12-13 20:31:49 +0900

    add custom data test

--- koai/__init__.py
+++ koai/__init__.py
@@ -1,6 +1,6 @@
 from . import benchmarks
 from .benchmarks import finetune
 
-__version__ = "0.0.1.3"
+__version__ = "0.0.1.4"
 
 __all__ = ["benchmarks", "finetune"] # more functions will be added soon...
