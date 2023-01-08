9e2b19f1fcb0386ab039782e75f9c6e3080bfc94
eddiejeon <eddie14@wisenut.co.kr>
2023-01-08 18:04:21 +0900

    fix-error

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from koai import finetune
+def test_fineune():
+    finetune(
+        "glue-cola",
+        "klue/bert-base",
+        do_train=True,
+        do_eval=True,
+        num_train_epochs=1,
+        evaluation_strategy="epoch",
+        save_strategy="no",
+        logging_strategy="epoch",
+        max_source_length=512,
+    )
 
-finetune(
-    "glue-cola",
-    "klue/bert-base",
-    do_train=True,
-    do_eval=True,
-    num_train_epochs=1,
-    evaluation_strategy="epoch",
-    save_strategy="no",
-    logging_strategy="epoch",
-    max_source_length=512,
-)
+def main():
+    assert test_fineune() == None
+
+if __name__=="__main__":
+    main()
