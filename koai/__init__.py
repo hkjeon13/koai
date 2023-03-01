a17b3d68800901c0587a0b1306c27036b761c6d3
eddiejeon <eddie14@wisenut.co.kr>
2023-03-01 17:43:23 +0900

    draft

--- koai/__init__.py
+++ koai/__init__.py
@@ -1,8 +1,7 @@
 from . import benchmarks
 from .benchmarks import finetune, TaskInfo
 from .utils import *
-from .rs_utils import *
 
-__version__ = "0.0.1.7"
+__version__ = "0.0.1.9"
 
 __all__ = ["benchmarks", "finetune", "TaskInfo"]  # more functions will be added soon...
--- test.py
+++ test.py
@@ -34,5 +34,6 @@ def main():
     a = torch.tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     print(torch.diag(a))
 
+
 if __name__ == "__main__":
     main()
