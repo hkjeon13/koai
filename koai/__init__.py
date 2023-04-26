ca2f06093f7957b59f40316ca1101e025c709666
eddie14 <eddie14@wisenut.co.kr>
2023-04-26 16:48:28 +0900

    draft

--- koai/__init__.py
+++ koai/__init__.py
@@ -2,6 +2,6 @@ from . import benchmarks
 from .benchmarks import finetune, TaskInfo
 from .utils import *
 
-__version__ = "0.0.2.7"
+__version__ = "0.0.2.8"
 
 __all__ = ["benchmarks", "finetune", "TaskInfo"]  # more functions will be added soon...
