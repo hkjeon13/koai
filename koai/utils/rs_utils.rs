0d5cfcfe1aa6b4b683674f03ab9390a9945e4f89
eddie14 <eddie14@wisenut.co.kr>
2023-04-26 10:42:10 +0900

    draft

--- koai/utils/rs_utils.rs
+++ koai/utils/rs_utils.rs
@@ -1,18 +1,18 @@
 extern crate pyo3;
 use std::collections::HashMap;
 use pyo3::prelude::*;
-use bm25::BM25;
-use text::sliding_text_sequence;
+use bm25;
+use text;
 
 #[pyfunction]
 fn sliding_texts(_py: Python, texts:Vec<String>, window_size:usize, stride:usize) -> PyResult<Vec<Vec<String>>>{
-    Ok(sliding_text_sequence(texts, window_size, stride))
+    Ok(text::sliding_text_sequence(texts, window_size, stride))
 }
 
 
 #[pymodule]
 fn rs_utils(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(sliding_texts))?;
-    m.add_class::<BM25>()?;
+    m.add_class::<bm25::BM25>()?;
     Ok(())
 }
