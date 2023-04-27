440283a5c82c6638e359ebc65bfee4b6978a367b
eddie14 <eddie14@wisenut.co.kr>
2023-04-27 12:52:39 +0900

    draft

--- koai/utils/lib.rs
+++ koai/utils/lib.rs
@@ -1,7 +1,7 @@
 extern crate pyo3;
 use std::collections::HashMap;
 use pyo3::prelude::*;
-
+use rayon::prelude::*;
 
 struct Token {
     text: String,
@@ -52,6 +52,7 @@ pub struct BM25 {
     b: f32,
 }
 
+
 #[pymethods]
 impl BM25 {
     #[new]
@@ -62,6 +63,15 @@ impl BM25 {
             k1: 1.2,
             b: 0.75,
         }
+    }
+    #[getter]
+    fn get_index(&self) -> PyResult<HashMap<String, Document>> {
+        Ok(self.index.clone())
+    }
+
+    #[getter]
+    fn get_token_index(&self) -> PyResult<HashMap<String, Token>> {
+        Ok(self.token_index.clone())
     }
 
     fn _calculate(&self, tokenized_query: Vec<String>, doc: &Document, avg_doc_length:f32) -> f32 {
@@ -77,6 +87,7 @@ impl BM25 {
         };
         score
     }
+
     fn search(&self, tokenized_query: Vec<String>, n: i32) -> PyResult<Vec<(String, f32)>> {
         let avg_doc_length = self.index.iter().map(|(_, doc)| doc.maps.values().sum::<i32>()).sum::<i32>() as f32 / self.index.len() as f32;
         let mut result = self.index.iter().map(|(id, doc)| {
@@ -87,6 +98,13 @@ impl BM25 {
             result.iter().take(n as usize).map(|x|x.to_owned())
             .collect::<Vec<(String, f32)>>()
         )
+    }
+
+    fn build_from_corpus(&mut self, tokenized_docs: Vec<(String, Vec<String>)>) -> PyResult<()>{
+        tokenized_docs.par_iter().for_each(|(id, tokenized_doc)| {
+            self.add_document(id.to_string(), tokenized_doc.to_vec());
+        });
+        Ok(())
     }
 
     fn add_document(&mut self, id:String, tokenized_doc: Vec<String>) {
@@ -115,6 +133,7 @@ impl BM25 {
         }
 
     }
+
     fn remove_document(&mut self, id:String) {
         self.index.remove(&id);
     }
